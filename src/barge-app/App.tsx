26b4a5a3c2606a0a4824e93573f535b0b463bf00
Half-Gray <halfgray7e@gmail.com>
2024-07-27 23:37:19 -0700

    Make component more complex for more realistic demonstration

--- src/barge-app/App.tsx
+++ src/barge-app/App.tsx
@@ -1,6 +1,26 @@
-import { h } from 'preact';
+import { h, Component } from 'preact';
 
-export function App() {
-	const text = 'Hello!';
-	return <p>{text}</p>;
+export class App extends Component<{}, { userName: string }> {
+	render({}, { userName }) {
+		const text = userName ? `Hello, ${userName}!` : 'Hello!';
+		return (
+			<div>
+				<label>
+					Name:{' '}
+					<input
+						type="text"
+						value={userName}
+						onInput={this.updateName.bind(this)}
+					/>
+				</label>
+				<p>{text}</p>
+			</div>
+		);
+	}
+	// Don't make this an arrow function because the generated code
+	// would trigger a polyfill for Array.prototype.concat.
+	// You must use .bind(this) when passing this function as an event handler.
+	updateName(e: Event) {
+		this.setState({ userName: (e.target as HTMLInputElement).value });
+	}
 }
