dbaa8edc8c2319542d6c010a96c61e7454402fbd
Half-Gray <halfgray7e@gmail.com>
2024-07-27 20:21:56 -0700

    Add polyfill config to Babel and plugin to error when polyfill is needed

--- src/barge-app/rollup.config.ts
+++ src/barge-app/rollup.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'rollup';
+import { defineConfig, Plugin } from 'rollup';
 import typescript from '@rollup/plugin-typescript';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
@@ -20,9 +20,23 @@ export default defineConfig({
 						targets: {
 							ie: '9',
 						},
+						useBuiltIns: 'usage',
+						corejs: '3.37',
 					},
 				],
 			],
 		}),
+		banCoreJs(),
 	],
 });
+
+function banCoreJs(): Plugin {
+	return {
+		name: 'rollup-plugin-ban-core-js',
+		resolveId(source) {
+			if (source === 'core-js' || source.startsWith('core-js/')) {
+				throw new Error(`core-js import detected: ${source}`);
+			}
+		},
+	};
+}
